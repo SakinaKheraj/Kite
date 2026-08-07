737e63e177a47828ae5e29fd868948557cc73e4f
Sakina <sakinakheraj@gmail.com>
2026-08-07 22:27:43 +0530

    Map /user/v1/getUser and /v1/users/{userId} endpoints in user-service

--- backend/user-service/src/main/java/com/example/userservice/controller/UserController.java
+++ backend/user-service/src/main/java/com/example/userservice/controller/UserController.java
@@ -1,20 +1,17 @@
 package com.example.userservice.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.example.userservice.entities.UserInfoDto;
 import com.example.userservice.service.UserService;
-
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.ResponseBody;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-
-
 
 @RestController
 public class UserController {
@@ -22,31 +19,39 @@ public class UserController {
     @Autowired
     private UserService userService;
 
-    @PostMapping("/auth/v1/createUpdate")
+    @PostMapping({"/auth/v1/createUpdate", "/v1/users"})
     public ResponseEntity<UserInfoDto> createUpdate(@RequestBody UserInfoDto userInfoDto) {
         try {
             UserInfoDto user = userService.createOrUpdateUser(userInfoDto);
-            return new ResponseEntity<>(user,HttpStatus.OK);
-        } catch(Exception e) {
+            return new ResponseEntity<>(user, HttpStatus.OK);
+        } catch (Exception e) {
             return new ResponseEntity<>(HttpStatus.NOT_FOUND);
         }
     }
 
-@GetMapping("/auth/v1/getUser")
-public ResponseEntity<UserInfoDto> getUser(
-        @RequestParam("user_id") String userId) {
+    @GetMapping({"/auth/v1/getUser", "/user/v1/getUser"})
+    public ResponseEntity<UserInfoDto> getUser(@RequestParam("user_id") String userId) {
+        try {
+            UserInfoDto dto = new UserInfoDto();
+            dto.setUserId(userId);
 
-    try {
-        UserInfoDto dto = new UserInfoDto();
-        dto.setUserId(userId);
-
-        UserInfoDto user = userService.getUser(dto);
-        return ResponseEntity.ok(user);
-
-    } catch (Exception e) {
-        e.printStackTrace();
-        return ResponseEntity.notFound().build();
+            UserInfoDto user = userService.getUser(dto);
+            return ResponseEntity.ok(user);
+        } catch (Exception e) {
+            return ResponseEntity.notFound().build();
+        }
     }
-}  
-    
+
+    @GetMapping("/v1/users/{userId}")
+    public ResponseEntity<UserInfoDto> getUserByPathId(@PathVariable("userId") String userId) {
+        try {
+            UserInfoDto dto = new UserInfoDto();
+            dto.setUserId(userId);
+
+            UserInfoDto user = userService.getUser(dto);
+            return ResponseEntity.ok(user);
+        } catch (Exception e) {
+            return ResponseEntity.notFound().build();
+        }
+    }
 }
